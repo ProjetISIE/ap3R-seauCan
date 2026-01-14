29d9effd590785c8c4f31ed2b47eed9c86158617
Guilhem Fauré <pro@guilhemfau.re>
2026-01-14 12:39:50 +0100

    fix: canmessage

--- src/CanMessage.cpp
+++ src/CanMessage.cpp
@@ -1,34 +1,31 @@
-#include "../include/CanManager.hpp"
+#include "CanMessage.hpp"
 
-bool CanManager::isMessageValid() const noexcept {
+bool CanMessage::isMessageValid() const noexcept {
 
     // Verif Format
-    switch(this->getFormat()) {
-        case canFormat::STD :
-            // Verif ID
-            if( this->getId() > 0h3FF){ 
-                return false;
-            }
-            break;
-        case canFormat::EXT :
-            // Verif ID
-            if( this->getId() > 0hFFFFFFF){ 
-                return false;
-            }
-            break;
-        case default :
+    switch (this->getFormat()) {
+    case CanMessage::canFormat::STD:
+        // Verif ID
+        if (this->getId() > 0x3FF) {
             return false;
-            break;
+        }
+        break;
+    case CanMessage::canFormat::EXT:
+        // Verif ID
+        if (this->getId() > 0xFFFFFFF) {
+            return false;
+        }
+        break;
+    default: return false; break;
     }
 
-    if(this->getDlc() > 8){
+    if (this->getDlc() > 8) {
         return false;
     }
 
-    if(this->getDlc() != this->Data().size()){
+    if (this->getDlc() != this->Data().size()) {
         return false;
     }
 
     return true;
-
 }

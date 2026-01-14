139b035cbd5c059b52c560be4b6188d8e200864b
Guilhem Fauré <pro@guilhemfau.re>
2026-01-14 12:00:06 +0100

    test: CanMessage ctor 1

--- test/CanMessage.cpp
+++ test/CanMessage.cpp
@@ -1,1 +1,19 @@
+#include "CanMessage.hpp"
 #include <doctest/doctest.h>
+#include <print>
+
+TEST_CASE("CanMessage 2 arg ctor") {
+    std::println("Testing CanMessage() 2 arg ctor");
+    {
+        CanMessage msg =
+            CanMessage(CanMessage::canType::DATA, CanMessage::canFormat::STD);
+        CHECK_EQ(msg.getType(), CanMessage::canType::DATA);
+        CHECK_EQ(msg.getFormat(), CanMessage::canFormat::STD);
+        CHECK_EQ(msg.getId(), 0);
+        CHECK_EQ(msg.getDlc(), 0);
+    }
+    CanMessage msg =
+        CanMessage(CanMessage::canType::REMOTE, CanMessage::canFormat::EXT);
+    CHECK_EQ(msg.getType(), CanMessage::canType::REMOTE);
+    CHECK_EQ(msg.getFormat(), CanMessage::canFormat::EXT);
+}

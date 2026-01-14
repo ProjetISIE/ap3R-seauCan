4c78baf6cee260b28475d982487d953d6df07510
Guilhem Fauré <pro@guilhemfau.re>
2026-01-14 12:12:17 +0100

    test: CanMessage isMessageValid and toString

--- test/CanMessage.cpp
+++ test/CanMessage.cpp
@@ -4,16 +4,13 @@
 
 TEST_CASE("CanMessage 2 arg ctor") {
     std::println("Testing CanMessage() 2 arg ctor");
-    {
-        CanMessage msg =
-            CanMessage(CanMessage::canType::DATA, CanMessage::canFormat::STD);
-        CHECK_EQ(msg.getType(), CanMessage::canType::DATA);
-        CHECK_EQ(msg.getFormat(), CanMessage::canFormat::STD);
-        CHECK_EQ(msg.getId(), 0);
-        CHECK_EQ(msg.getDlc(), 0);
-    }
     CanMessage msg =
-        CanMessage(CanMessage::canType::REMOTE, CanMessage::canFormat::EXT);
+        CanMessage(CanMessage::canType::DATA, CanMessage::canFormat::STD);
+    CHECK_EQ(msg.getType(), CanMessage::canType::DATA);
+    CHECK_EQ(msg.getFormat(), CanMessage::canFormat::STD);
+    CHECK_EQ(msg.getId(), 0);
+    CHECK_EQ(msg.getDlc(), 0);
+    msg = CanMessage(CanMessage::canType::REMOTE, CanMessage::canFormat::EXT);
     CHECK_EQ(msg.getType(), CanMessage::canType::REMOTE);
     CHECK_EQ(msg.getFormat(), CanMessage::canFormat::EXT);
 }
@@ -36,4 +33,22 @@ TEST_CASE("CanMessage 5 args ctor") {
     CHECK_EQ(msg.getFormat(), CanMessage::canFormat::STD);
     CHECK_EQ(msg.getId(), 1);
     CHECK_EQ(msg.getDlc(), MAX_DLC);
+}
+
+TEST_CASE("CanMessage isMessageValid()") {
+    std::println("Testing CanMessage::isMessageValid()");
+    CanMessage msg = CanMessage(1, CanMessage::canType::DATA,
+                                CanMessage::canFormat::STD, MAX_DLC);
+    CHECK(msg.isMessageValid());
+    msg = CanMessage(1, CanMessage::canType::DATA, CanMessage::canFormat::STD,
+                     MAX_DLC + 1);
+    CHECK_FALSE(msg.isMessageValid());
+}
+
+TEST_CASE("CanMessage toString()") {
+    std::println("Testing CanMessage::toString()");
+    CanMessage msg = CanMessage(1, CanMessage::canType::DATA,
+                                CanMessage::canFormat::STD, MAX_DLC);
+    // TODO à voir selon implémentation
+    CHECK_EQ(msg.toString(), "ID: 1, Type: DATA, Format: STD, DLC: 8");
 }

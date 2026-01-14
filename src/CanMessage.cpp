ef89a4c7f070f96dc91ca27e9e1d63677ac1ade2
anael <anael.tessier@etu.univ-tours.fr>
2026-01-14 12:33:12 +0100

    add isMessageValid()

--- src/CanMessage.cpp
+++ src/CanMessage.cpp
@@ -2,8 +2,33 @@
 
 bool CanManager::isMessageValid() const noexcept {
 
-    // Verif ID
-    if(this->format == canType::STD) {
-        if()
+    // Verif Format
+    switch(this->getFormat()) {
+        case canFormat::STD :
+            // Verif ID
+            if( this->getId() > 0h3FF){ 
+                return false;
+            }
+            break;
+        case canFormat::EXT :
+            // Verif ID
+            if( this->getId() > 0hFFFFFFF){ 
+                return false;
+            }
+            break;
+        case default :
+            return false;
+            break;
     }
+
+    if(this->getDlc() > 8){
+        return false;
+    }
+
+    if(this->getDlc() != this->Data().size()){
+        return false;
+    }
+
+    return true;
+
 }

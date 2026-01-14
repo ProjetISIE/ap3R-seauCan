93359b4c98c6f8d908cbe84519935b9d5095e178
Guilhem Fauré <pro@guilhemfau.re>
2026-01-14 12:05:00 +0100

    test: CanMessage ctor 2 and 3

--- test/CanMessage.cpp
+++ test/CanMessage.cpp
@@ -17,3 +17,23 @@ TEST_CASE("CanMessage 2 arg ctor") {
     CHECK_EQ(msg.getType(), CanMessage::canType::REMOTE);
     CHECK_EQ(msg.getFormat(), CanMessage::canFormat::EXT);
 }
+
+TEST_CASE("CanMessage 4 args ctor") {
+    std::println("Testing CanMessage() 3 args ctor");
+    CanMessage msg =
+        CanMessage(1, CanMessage::canType::DATA, CanMessage::canFormat::STD);
+    CHECK_EQ(msg.getType(), CanMessage::canType::DATA);
+    CHECK_EQ(msg.getFormat(), CanMessage::canFormat::STD);
+    CHECK_EQ(msg.getId(), 1);
+    CHECK_EQ(msg.getDlc(), 0);
+}
+
+TEST_CASE("CanMessage 5 args ctor") {
+    std::println("Testing CanMessage() 5 args ctor");
+    CanMessage msg = CanMessage(1, CanMessage::canType::DATA,
+                                CanMessage::canFormat::STD, MAX_DLC);
+    CHECK_EQ(msg.getType(), CanMessage::canType::DATA);
+    CHECK_EQ(msg.getFormat(), CanMessage::canFormat::STD);
+    CHECK_EQ(msg.getId(), 1);
+    CHECK_EQ(msg.getDlc(), MAX_DLC);
+}

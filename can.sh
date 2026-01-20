4eadce2bd552e67fb51aa581914785aa592d242e
Guilhem Fauré <pro@guilhemfau.re>
2026-01-20 14:18:20 +0100

    feat: can script

--- can.sh
+++ can.sh
@@ -1,6 +1,7 @@
 sudo modprobe can
 sudo modprobe can_raw
 sudo modprobe can_dev
+# sudo modprobe slcan
 sudo slcand -c -s1 /dev/ttyUSB0 slcan0
 sudo ip link set dev slcan0 up
 printf "Utiliser candump et cansend pour lire et écrire\n"

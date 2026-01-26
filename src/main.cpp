48307928bd7a59779d7308bac8c9804aba82f020
Guilhem Fauré <pro@guilhemfau.re>
2026-01-26 17:32:33 +0100

    feat: cleaner cli

--- src/main.cpp
+++ src/main.cpp
@@ -27,35 +27,35 @@ int main() {
                     dataBytes.push_back(
                         static_cast<uint8_t>(std::stoul(byteStr, nullptr, 16)));
                 }
-
+                // CAN Format: Standard or Extended
                 CanMessage::canFormat fmt = (id > 0x7FF)
                                                 ? CanMessage::canFormat::EXT
                                                 : CanMessage::canFormat::STD;
-
+                // CAN Type: DATA or REMOTE
                 size_t inputSize = dataBytes.size();
                 CanMessage::canType type = CanMessage::canType::DATA;
                 uint8_t dlc = 0;
                 if (inputSize == 0) type = CanMessage::canType::REMOTE;
                 else if (inputSize > 8) {
-                    std::println(
-                        stderr,
-                        "Warning: {} bytes provided, only first 8 sent, "
-                        "remaining {} ignored",
-                        inputSize, inputSize - 8);
+                    std::println(stderr,
+                                 "WARN: {} bytes provided, only first 8 sent, "
+                                 "remaining {} ignored",
+                                 inputSize, inputSize - 8);
                     dlc = 8;
                 } else dlc = static_cast<uint8_t>(inputSize);
-
+                // Create and emit complete CAN message
                 CanMessage msg(id, type, fmt, dlc);
                 auto& msgData = msg.Data();
                 for (size_t i = 0; i < dlc; ++i) msgData[i] = dataBytes[i];
                 manager.Emit(msg);
+                std::print("\n");
             } catch (const std::exception& e) {
                 std::println(stderr, "Invalid Input: {}", e.what());
             }
         } else {
             CanMessage rxMsg;
             while (manager.Receive(rxMsg))
-                std::print("\n\n{}\n\n", rxMsg.toString());
+                std::print("\n{}\n\n", rxMsg.toString());
         }
     }
     return 0;

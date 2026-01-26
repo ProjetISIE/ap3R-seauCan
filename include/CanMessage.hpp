ea6cbe4ff4adb3a155c3af257374638839e3f06e
Guilhem Fauré <pro@guilhemfau.re>
2026-01-26 19:29:40 +0100

    docs: comment slcanInterface

--- include/CanMessage.hpp
+++ include/CanMessage.hpp
@@ -9,14 +9,20 @@ constexpr uint8_t MAX_DLC{8};
 
 class CanMessage {
   public:
+    /**
+     * @brief Type of CAN message
+     */
     enum class canType {
-        DATA,
-        REMOTE,
+        DATA,  /**< Data Frame */
+        REMOTE, /**< Remote Transmission Request (RTR) Frame */
     };
 
+    /**
+     * @brief Format of CAN ID
+     */
     enum class canFormat {
-        STD,
-        EXT,
+        STD, /**< Standard Frame (11-bit ID) */
+        EXT, /**< Extended Frame (29-bit ID) */
     };
 
   private:
@@ -27,35 +33,130 @@ class CanMessage {
     std::array<uint8_t, MAX_DLC> _Data{0};
 
   public:
+    /**
+     * @brief Default Constructor
+     * Initializes an empty standard data frame with ID 0.
+     */
     CanMessage() = default;
-    CanMessage(const CanMessage&) = default;
-    CanMessage(CanMessage&&) = default;
-    CanMessage& operator=(const CanMessage&) = default;
-    CanMessage& operator=(CanMessage&&) = default;
+    
+    /**
+     * @brief Copy Constructor
+     * @param other Message to copy
+     */
+    CanMessage(const CanMessage& other) = default;
+
+    /**
+     * @brief Move Constructor
+     * @param other Message to move
+     */
+    CanMessage(CanMessage&& other) = default;
+
+    /**
+     * @brief Copy Assignment Operator
+     * @param other Message to copy
+     * @return Reference to this object
+     */
+    CanMessage& operator=(const CanMessage& other) = default;
+
+    /**
+     * @brief Move Assignment Operator
+     * @param other Message to move
+     * @return Reference to this object
+     */
+    CanMessage& operator=(CanMessage&& other) = default;
+
+    /**
+     * @brief Destructor
+     */
     virtual ~CanMessage() = default;
 
+    /**
+     * @brief Constructor with Type and Format
+     * @param type_ CAN Message Type (DATA/REMOTE)
+     * @param format_ CAN ID Format (STD/EXT)
+     */
     CanMessage(canType type_, canFormat format_)
         : Type{type_}, Format{format_} {}
+
+    /**
+     * @brief Constructor with ID, Type, and Format
+     * @param id_ CAN Identifier
+     * @param type_ CAN Message Type (DATA/REMOTE)
+     * @param format_ CAN ID Format (STD/EXT)
+     */
     CanMessage(uint32_t id_, canType type_, canFormat format_)
         : Id{id_}, Type{type_}, Format{format_} {}
+
+    /**
+     * @brief Full Constructor
+     * @param id_ CAN Identifier
+     * @param type_ CAN Message Type (DATA/REMOTE)
+     * @param format_ CAN ID Format (STD/EXT)
+     * @param dlc_ Data Length Code (0-8)
+     */
     CanMessage(uint32_t id_, canType type_, canFormat format_, uint8_t dlc_)
         : Id{id_}, Type{type_}, Format{format_}, Dlc{dlc_} {}
 
+    /**
+     * @brief Get the CAN Identifier
+     * @return uint32_t CAN ID
+     */
     [[nodiscard]] uint32_t getId() const noexcept { return this->Id; }
+
+    /**
+     * @brief Set the CAN Identifier
+     * @param id_ New CAN ID
+     */
     void setId(uint32_t id_) noexcept { this->Id = id_; }
 
+    /**
+     * @brief Get the CAN Message Type
+     * @return canType (DATA or REMOTE)
+     */
     [[nodiscard]] canType getType() const noexcept { return this->Type; }
+
+    /**
+     * @brief Set the CAN Message Type
+     * @param type_ New CAN Type
+     */
     void setType(canType type_) noexcept { this->Type = type_; }
 
+    /**
+     * @brief Get the CAN ID Format
+     * @return canFormat (STD or EXT)
+     */
     [[nodiscard]] canFormat getFormat() const noexcept { return this->Format; }
+
+    /**
+     * @brief Set the CAN ID Format
+     * @param format_ New CAN Format
+     */
     void setFormat(canFormat format_) noexcept { this->Format = format_; }
 
+    /**
+     * @brief Get the Data Length Code
+     * @return uint8_t DLC (0-8)
+     */
     [[nodiscard]] uint8_t getDlc() const noexcept { return this->Dlc; }
+
+    /**
+     * @brief Set the Data Length Code
+     * @param dlc_ New DLC (0-8)
+     */
     void setDlc(uint8_t dlc_) noexcept { this->Dlc = dlc_; }
 
+    /**
+     * @brief Get a mutable reference to the data array
+     * @return std::array<uint8_t, MAX_DLC>& Reference to data payload
+     */
     [[nodiscard]] std::array<uint8_t, MAX_DLC>& Data() noexcept {
         return this->_Data;
     }
+
+    /**
+     * @brief Get a const reference to the data array
+     * @return const std::array<uint8_t, MAX_DLC>& Const reference to data payload
+     */
     [[nodiscard]] const std::array<uint8_t, MAX_DLC>& Data() const noexcept {
         return this->_Data;
     }
--- src/slcanInterface.cpp
+++ src/slcanInterface.cpp
@@ -17,48 +17,47 @@
  * @brief Opens and binds CAN socket with timeout
  * @param ifname Interface name
  * @return Socket file descriptor
+ * Official documentation:
+ * https://www.kernel.org/doc/html/latest/networking/can.html#how-to-use-socketcan
  */
 static int openAndBindSocket(const std::string& ifname) {
+    // Open a SocketCAN Linux socket
     int s = socket(PF_CAN, SOCK_RAW, CAN_RAW);
     if (s < 0) throw std::runtime_error("Failed to open CAN socket");
-
-    struct ifreq ifr;
+    struct ifreq ifr; // Interface request with safe copy of interface name
     std::strncpy(ifr.ifr_name, ifname.c_str(), IFNAMSIZ - 1);
-    ifr.ifr_name[IFNAMSIZ - 1] = '\0';
+    ifr.ifr_name[IFNAMSIZ - 1] = '\0'; // Ensure interface name null-terminated
+    // Retrieve the interface index (required for binding) from system
     if (ioctl(s, SIOCGIFINDEX, &ifr) < 0) {
         int err = errno;
         close(s);
         throw std::runtime_error(std::format("Failed to find interface {}: {}",
                                              ifname, strerror(err)));
     }
-
+    // Initialise a clean socket address
     struct sockaddr_can addr;
     std::memset(&addr, 0, sizeof(addr));
     addr.can_family = AF_CAN;
     addr.can_ifindex = ifr.ifr_ifindex;
-
-    // Set receive timeout to 100ms
-    struct timeval tv;
-    tv.tv_sec = 0;
-    tv.tv_usec = 100000;
+    // Set receive timeout to 100ms so 'read' doesn't block forever
+    struct timeval tv{0, 100000};
     setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof tv);
-
+    // Bind the socket to the actual CAN interface
     if (bind(s, reinterpret_cast<struct sockaddr*>(&addr), sizeof(addr)) < 0) {
         int err = errno;
         close(s);
         throw std::runtime_error(std::format("Failed to bind socket to {}: {}",
                                              ifname, strerror(err)));
     }
-
-    return s;
+    return s; // Return the successfully opened and bound socket
 }
 
 void slcanInterface::receiveLoop(std::stop_token stoken) {
+    // Continuously read CAN frames from socket until (C++20) stop is requested
     while (!stoken.stop_requested()) {
         struct can_frame frame;
         ssize_t nbytes = read(this->sock, &frame, sizeof(struct can_frame));
         if (nbytes < 0) {
-            if (errno == EAGAIN || errno == EWOULDBLOCK) continue;
             std::println(stderr, "Error reading from CAN socket: {}",
                          strerror(errno));
         } else if (nbytes < static_cast<ssize_t>(sizeof(struct can_frame))) {
@@ -72,6 +71,7 @@ void slcanInterface::receiveLoop(std::stop_token stoken) {
 
 slcanInterface::slcanInterface()
     : ifname(defaultSlcanIfName), sock(openAndBindSocket(defaultSlcanIfName)),
+      // Start thread with lambda to capture 'this' and pass stop_token
       th([this](std::stop_token st) { this->receiveLoop(st); }) {
     std::println("Socket opened with fd: {} for interface {}", this->sock,
                  this->ifname);
@@ -79,53 +79,53 @@ slcanInterface::slcanInterface()
 
 slcanInterface::slcanInterface(std::string ifname_)
     : ifname(ifname_), sock(openAndBindSocket(ifname_)),
+      // Start thread with lambda to capture 'this' and pass stop_token
       th([this](std::stop_token st) { this->receiveLoop(st); }) {
     std::println("Socket opened with fd: {} for interface {}", this->sock,
                  this->ifname);
 }
 
+// More documentation:
+// https://www.kernel.org/doc/html/latest/networking/can.html#can-filter-usage-optimisation
 void slcanInterface::Emit(CanMessage& msg) {
-    struct can_frame frame;
+    struct can_frame frame; // Initialise a clean CAN frame
     std::memset(&frame, 0, sizeof(frame));
-
-    frame.can_id = msg.getId();
+    frame.can_id = msg.getId(); // Copy CanMessage ID to frame
     if (msg.getFormat() == CanMessage::canFormat::EXT)
-        frame.can_id |= CAN_EFF_FLAG;
-
+        frame.can_id |= CAN_EFF_FLAG; // Set Extended Frame Flag of frame
     if (msg.getType() == CanMessage::canType::REMOTE)
-        frame.can_id |= CAN_RTR_FLAG;
-
-    frame.can_dlc = msg.getDlc();
+        frame.can_id |= CAN_RTR_FLAG; // Set Remote Transmission Request Flag
+    frame.can_dlc = msg.getDlc();     // Copy CanMessage DLC to frame
+    // Copy CanMessage data to frame
     std::copy(msg.Data().begin(), msg.Data().end(), frame.data);
-
     if (write(this->sock, &frame, sizeof(struct can_frame)) !=
-        sizeof(struct can_frame)) {
+        sizeof(struct can_frame))
         throw std::runtime_error("Failed to write CAN frame to socket");
-    }
 }
 
 bool slcanInterface::Receive(CanMessage& msg) {
+    // Do not write to the queue while another thread is writing to it
     std::lock_guard<std::mutex> lock(this->queueMutex);
-    if (this->receiveQueue.empty()) return false;
-
-    auto received = this->receiveQueue.front();
-    if (received.can_id & CAN_EFF_FLAG) {
+    if (this->receiveQueue.empty()) return false; // No message available
+    auto oldestMsg = this->receiveQueue.front();
+    // Convert received CAN frame format to internal CanMessage format
+    if (oldestMsg.can_id & CAN_EFF_FLAG) { // Extended CAN
         msg.setFormat(CanMessage::canFormat::EXT);
-        msg.setId(received.can_id & CAN_EFF_MASK);
+        msg.setId(oldestMsg.can_id &
+                  CAN_EFF_MASK); // Mask out flags to get bare CAN ID
     } else {
         msg.setFormat(CanMessage::canFormat::STD);
-        msg.setId(received.can_id & CAN_SFF_MASK);
+        msg.setId(oldestMsg.can_id & CAN_SFF_MASK); // Standard CAN
     }
-
-    if (received.can_id & CAN_RTR_FLAG)
-        msg.setType(CanMessage::canType::REMOTE);
-    else msg.setType(CanMessage::canType::DATA);
-
-    msg.setDlc(received.can_dlc);
-    std::copy(std::begin(received.data), std::end(received.data),
+    // Convert received CAN frame type to internal CanMessage type
+    if (oldestMsg.can_id & CAN_RTR_FLAG)
+        msg.setType(CanMessage::canType::REMOTE); // Remote
+    else msg.setType(CanMessage::canType::DATA);  // Data
+    msg.setDlc(oldestMsg.can_dlc);                // Copy CAN DLC
+    std::copy(std::begin(oldestMsg.data), std::end(oldestMsg.data),
               msg.Data().begin());
-    this->receiveQueue.pop();
-    return true;
+    this->receiveQueue.pop(); // Remove the processed message from the queue
+    return true;              // Message sucessfully read
 }
 
 slcanInterface::~slcanInterface() {
